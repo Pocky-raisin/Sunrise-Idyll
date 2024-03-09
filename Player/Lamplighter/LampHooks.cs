693fe79c3616bd64f973b97b20426a23fe4fcd40
Pocky-raisin <160378936+Pocky-raisin@users.noreply.github.com>
2024-03-09 22:19:15 +0000

    GitHub said I had to commit

--- Player/Lamplighter/LampHooks.cs
+++ Player/Lamplighter/LampHooks.cs
@@ -34,6 +34,7 @@ namespace SunriseIdyll
         public static readonly SlugcatStats.Name LampName = new SlugcatStats.Name("IDYLL.LampScug", false);
         public static SlugBaseSaveData lampSaveData = SlugBase.SaveData.SaveDataExtension.GetSlugBaseData(new DeathPersistentSaveData(LampName));
 
+
         public static void Update(On.Player.orig_Update orig, Player self, bool e)
         {
             orig(self, e);
